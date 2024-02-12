e156d55e540bd2681780aa9d7fc1b5e0c21f4cf8
cvrqs <qqelpnw@gmail.com>
2024-02-12 17:36:14 +0300

    задача номер 4

--- qwe.py
+++ qwe.py
@@ -2,6 +2,7 @@ from flask import Flask, url_for, request, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/<title>')
 @app.route('/index/<title>')
 def prepare(title):
@@ -13,15 +14,26 @@ def prepare(title):
 def training(prof):
     return render_template('training.html', prof=prof)
 
+
 @app.route('/list_prof/<flag>')
 def prop(flag):
     proflist = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
-                   'инженер по терраформированию', 'климатолог',
-                   'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
-                   'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер',
-                   'штурман', 'пилот дронов']
+                'инженер по терраформированию', 'климатолог',
+                'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
+                'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер',
+                'штурман', 'пилот дронов']
 
     return render_template('works.html', flag=flag, proflist=proflist)
+
+
+@app.route('/answer')
+@app.route('/auto_answer')
+def answers():
+    data = {'title': 'Анкета', 'surname': 'Васильев', 'name': 'Дима', 'education': 'высшее', 'profession': 'Космонавт',
+            'sex': 'мужской', 'motivation': 'всегда мечтал покушать картошки с Марса', 'ready': 'готов'}
+
+    return render_template('auto_answer.html', **data)
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
