dd3ac207de9ab66324bcaf182572f31302037b81
cvrqs <qqelpnw@gmail.com>
2024-02-26 17:19:52 +0300

    job

--- mars/add_users.py
+++ mars/add_users.py
@@ -3,7 +3,7 @@ from flask import Flask
 from data import db_session
 from data.jobs import Jobs
 
-app = Flask(__name__)
+app = Flask(__name__) 
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 db_session.global_init("db/blogs.db")
 user = User()
