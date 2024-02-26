0c8823d420c2967094ee978d1774249c00d59f6b
cvrqs <qqelpnw@gmail.com>
2024-02-26 17:07:49 +0300

    добавил членов экипажа

--- mars/add_users.py
+++ mars/add_users.py
@@ -12,9 +12,44 @@ user.surname = "Ridley"
 user.age = 21
 user.position = "captain"
 user.speciality = "research engineer"
-user.address  = "module_1"
+user.address = "module_1"
 user.email = "scott_chief@mars.org"
 db_sess = db_session.create_session()
 db_sess.add(user)
 db_sess.commit()
 
+user = User()
+user.name = "Andrey"
+user.surname = "Ridl"
+user.age = 21
+user.position = "pilot"
+user.speciality = "little_research engineer"
+user.address = "module_2"
+user.email = "andy@mars.org"
+db_sess = db_session.create_session()
+db_sess.add(user)
+db_sess.commit()
+
+user = User()
+user.name = "Dima"
+user.surname = "Ridlov"
+user.age = 38
+user.position = "Cook"
+user.speciality = "little_research cook"
+user.address = "module_3"
+user.email = "dimas@mars.org"
+db_sess = db_session.create_session()
+db_sess.add(user)
+db_sess.commit()
+
+user = User()
+user.name = "Vasya"
+user.surname = "Roblox"
+user.age = 17
+user.position = "doctor"
+user.speciality = "little_research doctor"
+user.address = "module_4"
+user.email = "vasyan@mars.org"
+db_sess = db_session.create_session()
+db_sess.add(user)
+db_sess.commit()
